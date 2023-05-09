d7d4f9d17355d46f4246f2c103edbd7c1ac6caa9
Xaviontt <xavion@list.ru>
2023-05-10 00:30:50 +0300

    add activity_my_profile and activity_users

--- app/src/main/java/ru/itmo/multipleactivities/MainActivity.kt
+++ app/src/main/java/ru/itmo/multipleactivities/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.itmo.multipleactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val profileButton: Button = findViewById(R.id.profileButton)
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this, MyProfile::class.java)
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/ru/itmo/multipleactivities/Users.kt
@@ -0,0 +1,11 @@
+package ru.itmo.multipleactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Users : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
